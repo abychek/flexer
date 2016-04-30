c1537cb5fa8d64f087f53b7b3505bb58446053ae
abychek <alexey.bychek94@gmail.com>
2016-04-30 19:32:54 +0300

    Added User model. Configured User model. Configured security. Change users table migration.
    Some fixes.
    
    Configure fixtures
    
    Create User fixtures.
    
    problem fixed.

---
+++ src/AppBundle/DataFixtures/ORM/CustomerFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CustomerFixture implements FixtureInterface
+{
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setName('alexey');
+        $user->setUsername('vivaldiy');
+        $user->setPassword('q12we34r');
+        $user->setRole(['ROLE_CUSTOMER']);
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
