ab3e09476ffc9613b17ec5811f5fb430c468f935
abychek <alexey.bychek94@gmail.com>
2016-05-09 18:43:08 +0300

    Init

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Authentication\RegistrationBundle\AuthenticationRegistrationBundle(),
             new Api\EmployeeBundle\ApiEmployeeBundle(),
+            new Api\CustomerBundle\ApiCustomerBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
